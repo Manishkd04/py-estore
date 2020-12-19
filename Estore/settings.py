6f35d5047d056f49586a85b059670084bba16d47
Manishkd04 <60385624+Manishkd04@users.noreply.github.com>
2020-12-19 21:23:03 +0530

    Update settings.py

--- Estore/settings.py
+++ Estore/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = 'image/product/download/'
 MEDIA_ROOT = BASE_DIR
